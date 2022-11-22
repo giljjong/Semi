8a51b220a1db3836e1755733c71c0301e9db2bf4
limjangwook <vvovovv18@gamil.com>
2022-11-22 17:55:40 +0900

    2022-11-22

--- SemiProject/src/main/java/com/gdu/semi/controller/UserController.java
+++ SemiProject/src/main/java/com/gdu/semi/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.gdu.semi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
